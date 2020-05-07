94d884d23014d01d5493c2cc998d6da3c9cfd65f
Yatin Gupta <yatingupta7777@gmail.com>
2020-05-07 12:39:21 +0530

    small change

--- analyzer.py
+++ analyzer.py
@@ -5,7 +5,7 @@ def startsWithDate(s):
     pattern = '^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)(\d{2}|\d{4}), ([0-9][0-9]):([0-9][0-9]) -'
     result = re.match(pattern, s)
     if result:
-        return True
+L        return True
     return False
 
 
